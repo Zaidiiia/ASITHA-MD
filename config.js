3b4d9158a690a61aca0d0c506e9e44a7938e3752
Zaidiiia <165883939+Zaidiiia@users.noreply.github.com>
2024-10-03 23:23:47 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=7ddhXS7Y#kUu5D8lw5y98F-e-fCcKA9TBEiY0In992jIS6I92zGw",
+module.exports = {𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=7ddhXS7Y#kUu5D8lw5y98F-e-fCcKA9TBEiY0In992jIS6I92zGw",
 MONGODB: process.env.MONGODB || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=7ddhXS7Y#kUu5D8lw5y98F-e-fCcKA9TBEiY0In992jIS6I92zGw",//enter mongo db url
 };
